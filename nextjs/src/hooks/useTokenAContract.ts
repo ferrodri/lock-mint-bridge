48f32fcd6e8c25c088ef16051d960847c6336355
ferrodri <29060919+ferrodri@users.noreply.github.com>
2026-07-19 12:38:49 +0100

    useTokenAContract hook

---
+++ nextjs/src/hooks/useTokenAContract.ts
@@ -0,0 +1,19 @@
+'use client';
+
+import { useChainId } from 'wagmi';
+import { optimismSepolia } from 'wagmi/chains';
+
+interface ContractMap {
+  // chainId to contract address mapping
+  [chainId: number]: `0x${string}`;
+}
+
+export function useTokenAContract() {
+  const chainId = useChainId();
+
+  const contract: ContractMap = {
+    [optimismSepolia.id]: '0xE0C064077B513AfC0858287D75D3721cCf626C2e'
+  };
+
+  return contract[chainId];
+}
